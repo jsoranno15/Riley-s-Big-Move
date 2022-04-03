ea48cee110bfc279ac71c89138751f31d74cc638
dominique <dominiquemcc3@gmail.com>
2022-04-03 16:10:36 -0400

    MergedBinaryFiles

--- Assets/Code/AnxietyDrone.cs
+++ Assets/Code/AnxietyDrone.cs
@@ -8,20 +8,26 @@ public class AnxietyDrone : MonoBehaviour
 {
     public GameObject target;
     public float speed;
+    public float spotDistance = 20;
 
-    private Transform enemyTransform;
     private AudioSource guardAudio;
     private Animator animator;
-    private float escapeCounter = 0;
+
     private float spotCounter = 0;
     private bool playerIn = false;
+    private bool isDestroyed = false;
+
+    private int layerMask = 1 << 8;
 
     void Start()
     {
+        //Reset enemyTracker
+        PublicVars.enemyDestroyed = 0;
+
         guardAudio = GetComponent<AudioSource>();
-        enemyTransform = transform;
         animator = GetComponent<Animator>();
         animator.Play("Patrol");
+        layerMask = ~layerMask;
     }
 
     void OnTriggerEnter(Collider other)
@@ -30,8 +36,6 @@ public class AnxietyDrone : MonoBehaviour
         {
             Debug.Log("Possible Intruder encountered.");
             playerIn = true;
-            spotCounter = 0;
-            this.transform.GetComponent<NavMeshAgent>().isStopped = true;
         }
     }
 
@@ -39,17 +43,37 @@ public class AnxietyDrone : MonoBehaviour
     {
         if (playerIn == true)
         {
-            spotCounter += Time.deltaTime;
-            PublicVars.isSpotted = true;
-            Debug.Log("Intruder confirmed.");
-            enemyTransform.LookAt(target.transform);
-            enemyTransform.position = Vector3.MoveTowards(enemyTransform.position, target.transform.position, speed * Time.deltaTime);
-
-            if (PublicVars.isSpotted)
+            Debug.Log("Player in sight!");
+            Debug.Log(Vector3.Distance(target.transform.position, transform.position) + "from " + name);
+            Vector3 dirPlayer = (target.transform.position - transform.position).normalized;
+            RaycastHit raycastHit;
+            if (Physics.Raycast(transform.position, dirPlayer, out raycastHit, spotDistance, layerMask))
             {
-                PublicVars.isSpotted = false;
-                Debug.Log("Target has been eliminated.");
-                StartCoroutine(eliminateTarget());
+                Debug.Log("Theres a Ray!");
+                Debug.DrawLine(transform.position, raycastHit.point, Color.white);
+                if(raycastHit.collider.CompareTag("Player") && !isDestroyed)
+                {
+                    Debug.Log("This ray is hitting a player!");
+                    Debug.Log("Intruder confirmed.");
+                    transform.LookAt(target.transform);
+                    PublicVars.isSpotted = true;
+                    transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+                    
+                    //Below code is an alternative to Vector3.MoveTowards that allows the drones to move towards the player when they are spotted.
+                    //May need further testing for "proper implementation"
+                    //GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+                    //GetComponent<NavMeshAgent>().speed = speed * 3;
+                    
+                    //check to see if the player is close enough to the enemy to kill them AND the player has been confirmed to be spotted
+                    if (PublicVars.isSpotted && Vector3.Distance(target.transform.position, transform.position) < 1.5f)
+                    {
+                        Debug.Log(Vector3.Distance(target.transform.position, transform.position));
+                        PublicVars.isSpotted = false;
+                        Debug.Log("Target has been eliminated.");
+                        StartCoroutine(eliminateTarget());
+                    }
+                }
+                else { Debug.Log(raycastHit.collider.tag); }
             }
         }
     }
@@ -60,35 +84,47 @@ public class AnxietyDrone : MonoBehaviour
         {
             Debug.Log("Player is out of view");
             playerIn = false;
-            escapeCounter = 0;
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag.Equals("Player"))
+        if (collision.transform.CompareTag("Bullet"))
         {
-            Debug.Log("Intruder Eliminated.");
-            SceneManager.LoadScene(2);
-        }
-        else if (collision.transform.CompareTag("Bullet"))
-        {
-            PublicVars.enemyDestroyed += 1;
-            animator.Play("Shutdown");
-            StartCoroutine(selfDestruct());
+            if (!isDestroyed)
+            {
+                isDestroyed = true;
+                PublicVars.enemyDestroyed += 1;
+                Debug.Log("Num of enemies Destroyed is " + PublicVars.enemyDestroyed + " while number of enemies is " + PublicVars.enemyNum);
+                animator.Play("Shutdown");
+                StartCoroutine(selfDestruct());
+            }
         }
     }
     
     void Update()
     {
-        if (!playerIn) { escapeCounter += Time.deltaTime; }
-        if (playerIn) { spotCounter += Time.deltaTime; }
+        if (!playerIn) 
+        { 
+            spotCounter = 0; 
+            this.transform.GetComponent<NavMeshAgent>().isStopped = false;
+        }
+        else 
+        { 
+            spotCounter += Time.deltaTime;
+            //this.transform.GetComponent<NavMeshAgent>().isStopped = true; //may remove this depending on taste
+        }
 
-        if (escapeCounter == 3)
+        if (spotCounter == 3)
         {
             Debug.Log("Player has escaped, returning to normal patrol!");
-            this.transform.GetComponent<NavMeshAgent>().isStopped = false;
+            playerIn = false;
             
+        }
+
+        if(isDestroyed)
+        {
+            GetComponentInChildren<Light>().intensity -= 1;
         }
     }
 
